3541c1dbb12c12c69bda17ee4650c0a8965c804f
Hamdash <Hamdash@students.uni-marburg.de>
2021-04-17 19:07:27 +0100

    v1112

---
+++ TestClass.java
@@ -0,0 +1,1 @@
+import java.lang.reflect.Method;class TestClass {  boolean print(){  boolean x = "l"=="t"; return x;   }public static void print1(){System.out.println(" hallo ");}    public static void main(String[] args) { print1();}}
